c5ecad6d2cc1cc1e8fab21900a0423347f0b6079
Mbahmukong Destiny <fokundem.com@gmail.com>
2025-07-05 11:56:05 +0100

    changed url to prod

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://cfactorexiii.onrender.com'
-const API_BASE_URL='http://localhost:3000';
+const API_BASE_URL = 'https://cfactorexiii.onrender.com'
+// const API_BASE_URL='http://localhost:3000';
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
